8f9f9ce3463af55486fea10826a8c78548f9fee7
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-08 16:05:45 +0800

    Add files via upload

---
+++ index/index.js
@@ -0,0 +1,3 @@
+document.getElementById("img1").style.width = window.screen.width;
+document.getElementById("img2").style.width = window.screen.width;
+document.getElementById("img3").style.width = window.screen.width;
