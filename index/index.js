93c93527edc1ac418179a50eec8abc52f1ad71c5
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-08 16:22:28 +0800

    Update index.js
    浮点数感到有

--- index/index.js
+++ index/index.js
@@ -1,3 +1,3 @@
-document.getElementById("img1").style.width = window.screen.width;
-document.getElementById("img2").style.width = window.screen.width;
-document.getElementById("img3").style.width = window.screen.width;
+document.getElementById("img1").style.width = window.screen.availWidth ;
+document.getElementById("img2").style.width = window.screen.availWidth ;
+document.getElementById("img3").style.width = window.screen.availWidth ;
