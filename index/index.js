6cee4b6a6b4b6bc58772b2c1c5e93ce914a10693
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-08 21:04:25 +0800

    Update index.js
    防守打法

--- index/index.js
+++ index/index.js
@@ -1,3 +1,30 @@
-document.getElementById("img1").style.width = window.screen.availWidth ;
-document.getElementById("img2").style.width = window.screen.availWidth ;
-document.getElementById("img3").style.width = window.screen.availWidth ;
+image1 = document.getElementById("img1");
+image1_width = image1.style.width;
+image1_height = image1.style.height;
+
+console.log(image1_height);
+
+image1_width = window.screen.width+"px";
+document.getElementById("img2").style.width = window.screen.width+"px";
+
+
+
+
+var imag1 = document.getElementById("qjarea");
+
+// width =window.screen.width;
+// height = document.getElementById("img1").style.height;
+
+// console.log("宽度"+width);
+// console.log("高度"+height);
+// point1 = 0;
+// point2 = 0.345*height;
+// point3 = width;
+// point4 = 0.399*height;
+coords1= 0+","+0.31*2312+","+ window.screen.width+","+0.36*2312+"";
+// console.log(coords);
+imag1.setAttribute("coords",coords1)
+var imag2 = document.getElementById("xjarea");
+coords2= 0+","+0.36*2312+","+ window.screen.width+","+0.42*2312+"";
+// console.log(coords);
+imag2.setAttribute("coords",coords2)
