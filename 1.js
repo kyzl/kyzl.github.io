18a07a967e7627053492aa85b13592fdaa250301
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-16 14:43:04 +0800

    Update 1.js
    58676

--- 1.js
+++ 1.js
@@ -1,1 +1,15 @@
 
+// @name  学生请假
+// @run-at document-end
+
+(
+function(){
+	'use strict';
+	var domainStr = "http://e-office.nuist.edu.cn/taskcenter/wechat/appall";
+	var domain = window.location.href;
+	if(domainStr == domain){
+		var xsqjsq = document.querySelector('a[title="学生请假申请"]');
+		   xsqjsq.href = "http://e-office.nuist.edu.cn/infoplus/form/2492714/render";
+	}
+}
+)();
