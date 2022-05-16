36a31ae10dd1b0b68a655717ce6f4a1379708858
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-16 16:27:51 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,4 +1,3 @@
-
 // @name  学生请假
 // @run-at document-end
 
@@ -6,17 +5,12 @@
 function(){
 
 	let test2 = function(){
-		var domainStr = "http://e-office.nuist.edu.cn/infoplus/form/2576510/render?back=1";
-		var domain = window.location.href;
-		if(domainStr == domain){
 			var divE = document.getElementById("div_ewm_p");
 			divE.style.borderColor="#0000fe";
 			var img = document.querySelector("#div_ewm > img");
 			img.src="https://i.ibb.co/z66ZR6M/qr.png";
 			img.width= 243;
-		}
-	
-	setTimeout(test2,2000);
 	}
+	setTimeout(test2,2000);
 }
 )();
