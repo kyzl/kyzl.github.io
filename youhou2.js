3ecad47cc7f2401d049a17d3a752d45aeeff1ac9
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-16 12:32:21 +0800

    Update youhou2.js

--- youhou2.js
+++ youhou2.js
@@ -11,46 +11,29 @@ function(){
 		var lsh = document.getElementById("title_description_short");
 		lsh .innerText ='流水号:1875883';
 		
-		var sqrq = document.getElementById('label_0231ece2-3192-4fa2-af70-f9cbd9b012e4');
-		var date = new Date();
+		date = new Date();
 		month = date.getMonth() + 1;
 		if(month < 10){month="0"+month;}
 		day = date.getDate()-1;
 		if(day<10){day = "0"+day;}
-		sqrq.innerText = '2022-'+month+'-'+day;
+		var tContent = document.getElementsByClassName("infoplus_labelControlContainer inline");
+		tContent[0].innerText = '2022-'+month+'-'+day;
+		tContent[17].innerText = '2022-'+month+'-'+day;
+		tContent[19].innerText = '2022-'+month+'-'+(Number(day)+1);
+		tContent[24].innerText = '2022-'+month+'-'+day;
+		tContent[26].innerText = '2022-'+month+'-'+day;
+		tContent[28].innerText = '2022-'+month+'-'+day;
+		tContent[30].innerText = '2022-'+month+'-'+day;
 		
+		var tr = document.querySelector('.xdLayout > tbody');
+		var trChild = tr.childNodes;
+		for(let t =70;t<87;t++){
+			trChild[t].style.display = 'none';
+		}
 		
-		var jtlx = document.getElementById('label_d0948c32-7926-4855-b345-6fe283ffbe59');
-		jtlx.innerText = '2022-'+month+'-'+day;
-		var jtfx =document.getElementById('label_7fee0cd2-9991-4aa3-9c0f-0be4e5d8df66');
-		jtfx.innerText = '2022-'+month+'-'+(Number(day)+1);
-		
-		
-		var shrq = document.getElementById('label_d54277e5-bf5a-4e16-88a3-1241f6ef5d17');
-		shrq.innerText = '2022-'+month+'-'+day;
-		
-		var dsss = document.getElementById("label_7521010f-2dd9-4812-94af-1e7f208782f6");
-		dsss.innerText = '2022-'+month+'-'+day;
-		
-		var zn = document.getElementById("label_d54277e5-bf5a-4e16-88a3-1241f6ef5d17");
-		zn.innerText = '2022-'+month+'-'+day;
-		
-		var zcy = document.getElementById("label_d9e28365-9aca-46cc-99cd-4f7c4206afb8");
-		zcy.innerText ='2022-'+month+'-'+day;
-		
-		var gh = document.getElementById("label_3ff05105-da7d-48d8-8720-c7897643d3d3");
-		gh.innerText = '2022-'+month+'-'+day;
-		
-		var xjsj = document.getElementById("label_53905014-5872-4ffe-9df4-00206957cc67");
-		xjsj.innerText =  '2022-'+month+'-'+(Number(day)+1);
-		
-		
-		var shsj =document.getElementById("label_15421907-b32a-4ed2-941a-a6f4511c3176");
-		shsj.innerText = '2022-'+month+'-'+day;
 		
 		
 		var lisheItem= document.getElementsByClassName("remark_item");
-		
 		
 		lisheItem[0].style.display = 'none';
 		lisheItem[1].style.display = 'none';
@@ -65,6 +48,8 @@ function(){
 			time = shijian[i].innerText.split(' ')[1];
 			shijian[i].innerText = 	'2022-'+month+'-'+day+" "+time ;
 		}
+		
+		
 	}
 }
 )();
