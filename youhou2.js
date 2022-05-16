d4a4122c00089e3eb69a2fb3f03c87793e17341b
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-16 14:32:10 +0800

    Update youhou2.js

--- youhou2.js
+++ youhou2.js
@@ -8,55 +8,67 @@ function(){
 	if(domainStr1 == domain){
 		var xsqjsq = document.querySelector('a[title="学生请假申请"]');
 		   xsqjsq.href = "http://e-office.nuist.edu.cn/infoplus/form/2492714/render";
-		   alert(12);
 	}
 	
 	
 	var domainStr2 = "http://e-office.nuist.edu.cn/infoplus/form/2492714/render";
 	if(domainStr2 == domain){
+		
+		
 		var title_content = document.getElementById("title_content");
-		title_content .innerText = '学生请假申请:申请人(已通过)';
-		var lsh = document.getElementById("title_description_short");
-		lsh .innerText ='流水号:1875883';
 		
-		date = new Date();
-		month = date.getMonth() + 1;
-		if(month < 10){month="0"+month;}
-		day = date.getDate()-1;
-		if(day<10){day = "0"+day;}
-		var tContent = document.getElementsByClassName("infoplus_labelControlContainer inline");
-		tContent[0].innerText = '2022-'+month+'-'+day;
-		tContent[17].innerText = '2022-'+month+'-'+day;
-		tContent[19].innerText = '2022-'+month+'-'+(Number(day)+1);
-		tContent[24].innerText = '2022-'+month+'-'+day;
-		tContent[26].innerText = '2022-'+month+'-'+day;
-		tContent[28].innerText = '2022-'+month+'-'+day;
-		tContent[30].innerText = '2022-'+month+'-'+day;
-		
-		var tr = document.querySelector('.xdLayout > tbody');
-		var trChild = tr.childNodes;
-		for(let t =70;t<87;t++){
-			trChild[t].style.display = 'none';
+		while(true){
+			if(title_content != null){
+				
+				title_content .innerText = '学生请假申请:申请人(已通过)';
+				var lsh = document.getElementById("title_description_short");
+				lsh .innerText ='流水号:1875883';
+				
+				date = new Date();
+				month = date.getMonth() + 1;
+				if(month < 10){month="0"+month;}
+				day = date.getDate()-1;
+				if(day<10){day = "0"+day;}
+				var tContent = document.getElementsByClassName("infoplus_labelControlContainer inline");
+				tContent[0].innerText = '2022-'+month+'-'+day;
+				tContent[17].innerText = '2022-'+month+'-'+day;
+				tContent[19].innerText = '2022-'+month+'-'+(Number(day)+1);
+				tContent[24].innerText = '2022-'+month+'-'+day;
+				tContent[26].innerText = '2022-'+month+'-'+day;
+				tContent[28].innerText = '2022-'+month+'-'+day;
+				tContent[30].innerText = '2022-'+month+'-'+day;
+				
+				var tr = document.querySelector('.xdLayout > tbody');
+				var trChild = tr.childNodes;
+				for(let t =70;t<86;t++){
+					trChild[t].style.display = 'none';
+				}
+				
+				
+				
+				var lisheItem= document.getElementsByClassName("remark_item");
+				
+				lisheItem[0].style.display = 'none';
+				lisheItem[1].style.display = 'none';
+				lisheItem[2].style.display = 'none';
+				lisheItem[3].style.display = 'none';
+				lisheItem[9].style.display = 'none';
+				lisheItem[10].style.display = 'none';
+				lisheItem[11].style.display = 'none';
+				
+				var shijian = document.getElementsByClassName("form_remark_time");
+				for(let i = 4;i<9;i++){
+					time = shijian[i].innerText.split(' ')[1];
+					shijian[i].innerText = 	'2022-'+month+'-'+day+" "+time ;
+				}
+				
+				break;
+			}
+				
+			else{
+				title_content = document.getElementById("title_content");
+			}
 		}
-		
-		
-		
-		var lisheItem= document.getElementsByClassName("remark_item");
-		
-		lisheItem[0].style.display = 'none';
-		lisheItem[1].style.display = 'none';
-		lisheItem[2].style.display = 'none';
-		lisheItem[3].style.display = 'none';
-		lisheItem[9].style.display = 'none';
-		lisheItem[10].style.display = 'none';
-		lisheItem[11].style.display = 'none';
-		
-		var shijian = document.getElementsByClassName("form_remark_time");
-		for(let i = 4;i<9;i++){
-			time = shijian[i].innerText.split(' ')[1];
-			shijian[i].innerText = 	'2022-'+month+'-'+day+" "+time ;
-		}
-		
 		
 	}
 }
