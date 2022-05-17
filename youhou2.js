0533faeb5fad26cc0997df2a89a2e150a77fdccb
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-17 11:23:54 +0800

    Update youhou2.js
    nihao

--- youhou2.js
+++ youhou2.js
@@ -1,75 +1,79 @@
-// @name  学生请假
+// @name  studentQinJia
 // @run-at document-end
+// @match e-office.nuist.edu.cn
+// @require https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
 
 (
 function(){
-	var domainStr1 = "http://e-office.nuist.edu.cn/taskcenter/wechat/appall";
+
+	var domainStr = "http://e-office.nuist.edu.cn/taskcenter/wechat/appall";
 	var domain = window.location.href;
-	if(domainStr1 == domain){
-		var xsqjsq = document.querySelector('a[title="学生请假申请"]');
-		   xsqjsq.href = "http://e-office.nuist.edu.cn/infoplus/form/2492714/render";
+	
+	// 1.学生申请请假页面  将学生申请请假中的链接给更改掉
+	let studentQJ = function(){
+		 var xsqjsq = document.querySelector('a[title="学生请假申请"]');
+		 xsqjsq.href = "http://e-office.nuist.edu.cn/infoplus/form/2492714/render";
 	}
 	
+	if(domainStr == domain){
+		studentQJ();
+	}
 	
-	var domainStr2 = "http://e-office.nuist.edu.cn/infoplus/form/2492714/render";
-	if(domainStr2 == domain){
-		
-		
+	var domainQjTable = "http://e-office.nuist.edu.cn/infoplus/form/2492714/render";
+	// 2.点击进入请假表之后  修改请假表
+	let qjTable = function(){
+		// 修改页面最上面的信息  颜色是：#01acc0  文字:学生请假申请：申请人（提交：XXX）
 		var title_content = document.getElementById("title_content");
+		title_content .innerText = '学生请假申请:申请人(已通过)';
+		var lsh = document.getElementById("title_description_short");
+		lsh.innerText ='流水号:1875883';
+		var titleHolder = document.getElementById("title_holder");
+		titleHolder.style.backgroundColor = "#01acc0";
 		
-		while(true){
-			if(title_content != null){
-				
-				title_content .innerText = '学生请假申请:申请人(已通过)';
-				var lsh = document.getElementById("title_description_short");
-				lsh .innerText ='流水号:1875883';
-				
-				date = new Date();
-				month = date.getMonth() + 1;
-				if(month < 10){month="0"+month;}
-				day = date.getDate()-1;
-				if(day<10){day = "0"+day;}
-				var tContent = document.getElementsByClassName("infoplus_labelControlContainer inline");
-				tContent[0].innerText = '2022-'+month+'-'+day;
-				tContent[17].innerText = '2022-'+month+'-'+day;
-				tContent[19].innerText = '2022-'+month+'-'+(Number(day)+1);
-				tContent[24].innerText = '2022-'+month+'-'+day;
-				tContent[26].innerText = '2022-'+month+'-'+day;
-				tContent[28].innerText = '2022-'+month+'-'+day;
-				tContent[30].innerText = '2022-'+month+'-'+day;
-				
-				var tr = document.querySelector('.xdLayout > tbody');
-				var trChild = tr.childNodes;
-				for(let t =70;t<86;t++){
-					trChild[t].style.display = 'none';
-				}
-				
-				
-				
-				var lisheItem= document.getElementsByClassName("remark_item");
-				
-				lisheItem[0].style.display = 'none';
-				lisheItem[1].style.display = 'none';
-				lisheItem[2].style.display = 'none';
-				lisheItem[3].style.display = 'none';
-				lisheItem[9].style.display = 'none';
-				lisheItem[10].style.display = 'none';
-				lisheItem[11].style.display = 'none';
-				
-				var shijian = document.getElementsByClassName("form_remark_time");
-				for(let i = 4;i<9;i++){
-					time = shijian[i].innerText.split(' ')[1];
-					shijian[i].innerText = 	'2022-'+month+'-'+day+" "+time ;
-				}
-				
-				break;
-			}
-				
-			else{
-				title_content = document.getElementById("title_content");
-			}
-		}
-		
+		var noaction = document.getElementsByClassName("no_action");
+		noaction[0].innerText = "已通过审核";
 	}
+	
+	// 当请假页面出现： 加载完成(100%)  执行代码
+	// 思路  ：   1. 每隔1秒运行 qjTable一次  直到请假表格刷出来
+    //           2. 使用监听机制
+	
+	
+	var divLoader = document.getElementById("div_loader");
+	
+	// // 事件处理器
+	function callback(mutationRecords, observer){
+	    mutationRecords.forEach(mutationRecord => {
+	        // console.dir(mutationRecord)
+	        if(mutationRecord.type === "attributes") {
+				// return console.log('属性发生了变化 target =', mutationRecord.target);
+				
+				// 如果属性发生了变化 修改表格
+				qjTable();
+			}
+			// if(mutationRecord.type === 'childList') return console.log('添加or删除了 childList: ',mutationRecord.target)
+	        // if(mutationRecord.type === 'characterData') return console.log('文本节点的数据发生了变化',mutationRecord.target)
+	    })
+	}
+	
+	const observer = new MutationObserver(callback);
+	/*
+	 * 配置文件
+	*/
+	const config = {
+	    attributes: true,
+	    attributeFilter: undefined, /*需要监听的属性名称列表，如果没有表示监听全部的属性*/
+	    attributeOldValue: true, /*传递之前旧的值给mutationRecord*/
+	    characterData: false, /*是否监听内部文本节点的数据变化*/
+	    characterDataOldValue: false, /*mutationRecord 是否包含内部文本节点变化前的数据*/
+	    childList: false,
+	    subtree: false /*是否把监听的方位放到节点树中的全部子节点上*/
+	};
+	
+	// 进入请假页面后执行
+	if(domainQjTable == domain){
+		observer.observe(divLoader, config);
+	}
+	
 }
 )();
