276355251e8113ae75e28d60b5d73c0d507c4960
kyzl <85014093+kyzl@users.noreply.github.com>
2022-05-17 14:01:02 +0800

    Update xsqjsq.js

--- xsqjsq.js
+++ xsqjsq.js
@@ -1,1 +1,18 @@
 
+// ==UserScript==
+// @name 学生请假办理
+// @run-at document-start
+// @match http://e-office.nuist.edu.cn/infoplus/form/XSQXJSQ/start?back=1&x_posted=true
+// @description 学生请假办理
+// @version 0.1
+// ==/UserScript==
+(
+function(){
+
+	// 1.学生申请请假页面  将学生申请请假中的链接给更改掉
+	let studentQJ = function(){
+		window.location.href = "http://e-office.nuist.edu.cn/infoplus/form/2492714/render";
+	}
+	
+}
+)();
